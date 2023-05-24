ed60cd43d296700d68d14372091f53c37e98febe
MsKayMach <khanani.machu@gmail.com>
2023-05-24 07:47:59 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,15 @@
 #include "main.h"
-
-
 /**
  * main - Entry point
- * Description: 'print_alphabet' - print all alphabet in lowercase
- *Return: Always 0 (Success)
+ * Description for function: 'print_alphabet - print all alphabet in lowercase'
+ * Return: Always 0 (Success)
  */
+void print_alphabet(void)
+{
+	char letter;
 
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}
-
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -2,7 +2,8 @@
 
 /**
  * main - check the code
- *
+ * Description for function: print alphabets in lower case
+ * @c: The character to print
  * Return: Always 0.
  */
 int main(void)
